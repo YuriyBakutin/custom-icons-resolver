0bdb8d4cd4215218b347a4476e42db6c6c83e816
Yuriy Bakutin <yuriybakutin@gmail.com>
2021-09-04 12:03:20 +0300

    Refactoring index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,4 @@
-interface ImportInfo {
-  name?: string;
-  importName?: string;
-  path: string;
-}
-declare type SideEffectsInfo = (ImportInfo | string)[] | ImportInfo | string | undefined;
-interface ComponentInfo extends ImportInfo {
-  sideEffects?: SideEffectsInfo;
-}
-declare type ComponentResolveResult = string | ComponentInfo;
-declare type ComponentResolver = (name: string) => ComponentResolveResult | null | undefined | void;
+import ComponentResolver from 'unplugin-vue-components'
 
 export function customIconsResolver(options?: {
   prefix?: string,

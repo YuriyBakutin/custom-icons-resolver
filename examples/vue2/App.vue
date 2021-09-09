ed4ec62c6248a9d2b9d11a4b313a123f579b7dbb
Yuriy Bakutin <yuriybakutin@gmail.com>
2021-09-10 01:47:23 +0300

    Changed the Vue 2 example

--- examples/vue2/App.vue
+++ examples/vue2/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div style="color: brown; font-size: 100px">
-    <span style="color: rgb(88, 88, 88); font-size: 60px; margin-right: 40px">
-      Cat <custom-icon-cat-head />
-    </span>
-    <custom-icon-dog-head /> Dog
+  <div style="color: rgb(88, 88, 88); font-size: 60px; margin-right: 40px">
+    Cat <custom-icon-cat-head />
+    <custom-icon-dog-head class="big-brown" /> Dog
   </div>
 </template>
+<style>
+.big-brown {
+  color: brown;
+  font-size: 100px
+}
+</style>
